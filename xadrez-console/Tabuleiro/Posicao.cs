57ac3ed1ed28e2a638cae3b6967212db9a533475
Gabriel Pedrico <gabriel.pedrico@fatec.sp.gov.br>
2021-01-15 08:35:51 -0300

    Criando a classe Posicao

---
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna) // Construtor para receber Linha e Coluna
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString() // Funcao para definir como sera o formato de sua impressao para o usuario com Console.WriteLine();
+        {
+            return linha // Retorna o valor da linha concatenado com uma "," e o valor da coluna
+            + ","
+            + coluna;
+        }
+
+
+    }
+}
